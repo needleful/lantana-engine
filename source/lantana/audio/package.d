df7cae71c96cdc76cb37509cc66716ce399e6c2d
needleful <needleful@outlook.com>
2020-11-09 02:41:07 -0600

    Making a game-agnostic library

--- source/lantana/audio/package.d
+++
@@ -1,9 +0,0 @@
-// Part of the Lantana Engine
-// developed by needleful
-// Licensed under GPL v3.0
-
-module lantana.audio;
-
-import std.concurrency;
-
-public import lantana.audio.driver: AudioManager;
